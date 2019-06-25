d748b0714f6082d06e2d2ec8f0be94cf3cf9cb4f
Daniel Burt <gromitzy@gmail.com>
2019-06-25 04:00:22 +0200

    Add dash & slowed movement when holding shift

--- Pure Colors/Assets/Scripts/PlayerInput.cs
+++ Pure Colors/Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string horizontalAxis = "Horizontal";
+    [SerializeField] private string verticalAxis = "Vertical";
+    public string shootButton = "Mouse1";
+    public string slashButton = "Mouse2";
+    public string dashButton = "Dash";
+    public string slowDownButton = "Slowdown";
+
+    //A vector representing the 2D direction in which
+    internal Vector2 inputVector = new Vector2();
+    public Vector2 cursorPosition;
+    private Camera maincam;
+    
+    private void Start()
     {
-        
+        maincam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButton(slowDownButton)) print("slow");
+        inputVector = new Vector2(Input.GetAxisRaw(horizontalAxis), Input.GetAxisRaw(verticalAxis));
+    }
+
+    public Vector2 GetCursorPosition()
+    {
+        return maincam.ScreenToWorldPoint(Input.mousePosition);
     }
 }
