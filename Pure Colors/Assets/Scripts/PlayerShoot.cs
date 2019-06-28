e2bc674dd597d78684fc4cf1281044ea030adb32
Daniel Burt <gromitzy@gmail.com>
2019-06-28 04:07:24 +0200

    Add more test sprites, hook up world's least impressive sword swoosh

--- Pure Colors/Assets/Scripts/PlayerShoot.cs
+++ Pure Colors/Assets/Scripts/PlayerShoot.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [Header("Shooting")]
     public GameObject projectile;
     public float shootSpeed = 0.1f; //time between projectiles
+    [Header("Slashing")]
+    public GameObject swordSlash;
+    [Range(0, 1)]
+    [SerializeField] private float  slashOffset;
+    [SerializeField] private float slashDuration;
+    [SerializeField] private float slashCooldown;
+
+    private float lastSlash = 0;
+    
     private PlayerInput _input;
 
     void Start()
@@ -16,6 +26,7 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SwordSlash(_input.GetAimDirection(transform.position, _input.GetCursorPosition()));
         Shoot(_input.shootButton);
     }
     private void GetComponents()
@@ -44,4 +55,24 @@ public class PlayerShoot : MonoBehaviour
             yield return new WaitForSeconds(shootSpeed);
         }
     }
+
+    public void SwordSlash(Vector2 direction)
+    {
+        if(direction == Vector2.zero) direction = Vector2.right; //Edgecase but might as well
+        swordSlash.transform.localPosition = direction * slashOffset;
+        swordSlash.transform.up = direction;
+        if(Input.GetButtonDown(_input.slashButton) && Time.time > lastSlash + slashCooldown)
+        {
+            lastSlash = Time.time;
+        }
+        if(lastSlash + slashDuration > Time.time && lastSlash != 0)
+        {
+            swordSlash.SetActive(true);            
+        }
+        else
+        {
+            swordSlash.SetActive(false);
+        }
+    }
+
 }
